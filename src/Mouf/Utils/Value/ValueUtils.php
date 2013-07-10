8e969f73ce18c3e70efbca6b33b85609bb996a6a
David Négrier <d.negrier@thecodingmachine.com>
2013-07-10 19:27:51 +0200

    Adding support to arrays of ValueInterface

--- src/Mouf/Utils/Value/ValueUtils.php
+++ src/Mouf/Utils/Value/ValueUtils.php
@@ -11,13 +11,17 @@ class ValueUtils {
 	/**
 	 * Takes the value in parameter.
 	 * If this is a ValueInterface object, returns the val().
+	 * If this an array (or recursive array) containing ValueInterface objects, apply to the objects the val() method.
 	 * Otherwise, returns the value passed in parameter.
 	 * 
 	 * @param mixed $value
+	 * @return mixed
 	 */
 	public static function val($value) {
 		if ($value instanceof ValueInterface) {
 			return $value->val();
+		} elseif (is_array($value)) {
+			return array_map(function($item) { return ValueUtils::val($item); }, $value);
 		} else {
 			return $value;
 		}
